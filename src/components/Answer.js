c699942b921fe27a77b2747c4a4fd8d8d374069e
Mk Adore <mkador169@gmil.com>
2022-10-12 02:46:58 +0600

    view icon added

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Option from './Option';
+import { EyeIcon } from '@heroicons/react/24/solid';
 
 const Answer = ({questionAndOption}) => {
     const {id,question,options,correctAnswer} = questionAndOption;
@@ -15,13 +16,21 @@ const Answer = ({questionAndOption}) => {
         <div>
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-screen-sm sm:text-center sm:mx-auto">
+        <h6 className="mb-4 font-sans text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl sm:leading-none">
+        <div dangerouslySetInnerHTML={{__html:question}}/>
+        </h6>
+        <p className="text-base text-gray-700 md:text-lg sm:px-4">
+        {
+            options.map((option,i)=> <Option key={i} option={option} showAnswer={showAnswer}></Option>)
+        }
+        </p>
         <a
-          href="/"
+        //   href="/quiz"
           aria-label="View"
           className="inline-block mb-5 rounded-full sm:mx-auto"
         >
           <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-            <svg
+            {/* <svg
               className="w-12 h-12 text-deep-purple-accent-400"
               stroke="currentColor"
               viewBox="0 0 52 52"
@@ -33,17 +42,10 @@ const Answer = ({questionAndOption}) => {
                 fill="none"
                 points="29 13 14 29 25 29 23 39 38 23 27 23"
               />
-            </svg>
+            </svg> */}
+            <EyeIcon className="h-6 w-6 text-blue-500"/>
           </div>
         </a>
-        <h6 className="mb-4 font-sans text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl sm:leading-none">
-        <div dangerouslySetInnerHTML={{__html:question}}/>
-        </h6>
-        <p className="text-base text-gray-700 md:text-lg sm:px-4">
-        {
-            options.map((option,i)=> <Option key={i} option={option} showAnswer={showAnswer}></Option>)
-        }
-        </p>
         <hr className="w-full my-8 border-gray-300" />
       </div>
     </div>
