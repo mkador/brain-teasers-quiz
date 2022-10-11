b287324c38e513c6206c34f083d83a099f4fe0c8
Mk Adore <mkador169@gmil.com>
2022-10-11 16:01:43 +0600

    blog design added

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,7 +3,122 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1>Bolg</h1>
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid max-w-2xl mx-auto">
+        <div className="flex">
+          <div className="flex flex-col items-center mr-6">
+            <div className="w-px h-10 opacity-0 sm:h-full" />
+            <div>
+              <div className="flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full">
+                1
+              </div>
+            </div>
+            <div className="w-px h-full bg-gray-300" />
+          </div>
+          <div className="flex flex-col pb-6 sm:items-center sm:flex-row sm:pb-0">
+            <div className="sm:mr-5">
+              <div className="flex items-center justify-center w-16 h-16 my-3 rounded-full bg-indigo-50 sm:w-24 sm:h-24">
+                <svg
+                  className="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div>
+              <p className="text-xl font-semibold sm:text-base text-amber-700">
+                Q: What is the purpose of react router?
+              </p>
+              <p className="text-sm text-gray-700">
+              React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="flex flex-col items-center mr-6">
+            <div className="w-px h-10 bg-gray-300 sm:h-full" />
+            <div>
+              <div className="flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full">
+                2
+              </div>
+            </div>
+            <div className="w-px h-full bg-gray-300" />
+          </div>
+          <div className="flex flex-col pb-6 sm:items-center sm:flex-row sm:pb-0">
+            <div className="sm:mr-5">
+              <div className="flex items-center justify-center w-16 h-16 my-3 rounded-full bg-indigo-50 sm:w-24 sm:h-24">
+                <svg
+                  className="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div>
+              <p className="text-xl font-semibold sm:text-base text-amber-700">Q: How does context api work?</p>
+              <p className="text-sm text-gray-700">
+              In a typical React application, data is passed top-down (parent to child) via props, but such usage can be cumbersome for certain types of props (e.g. locale preference, UI theme) that are required by many components within an application. Context provides a way to share values like these between components without having to explicitly pass a prop through every level of the tree.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="flex flex-col items-center mr-6">
+            <div className="w-px h-10 bg-gray-300 sm:h-full" />
+            <div>
+              <div className="flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full">
+                3
+              </div>
+            </div>
+            <div className="w-px h-full opacity-0" />
+          </div>
+          <div className="flex flex-col pb-6 sm:items-center sm:flex-row sm:pb-0">
+            <div className="sm:mr-5">
+              <div className="flex items-center justify-center w-16 h-16 my-3 rounded-full bg-indigo-50 sm:w-24 sm:h-24">
+                <svg
+                  className="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div>
+              <p className="text-xl font-semibold sm:text-base text-amber-700">Q: What is the use of useref hook?</p>
+              <p className="text-sm text-gray-700">
+              The useRef Hook allows you to persist values between renders. <br />
+              It can be used to store a mutable value that does not cause a re-render when updated. <br />
+              It can be used to access a DOM element directly.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
         </div>
     );
 };
