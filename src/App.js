fc5568a95c91c3def5e850cb0d8c0063c1a304f8
Mk Adore <mkador169@gmil.com>
2022-10-12 14:22:51 +0600

    home location error updated

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ const router = createBrowserRouter([
     errorElement:<ErrorPage></ErrorPage>,
     children:[
       {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
         path:'/home',
         element:<Home></Home>
       },
